ed232983e69944f11f830993866c3155be1d3d4f
Lillian <lilliw2@uci.edu>
2019-02-27 18:29:30 -0800

    added action bar to grocery activity

--- app/src/main/java/watermelons/myfridge/GroceryActivity.java
+++ app/src/main/java/watermelons/myfridge/GroceryActivity.java
@@ -1,6 +1,7 @@
 package watermelons.myfridge;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class GroceryActivity extends AppCompatActivity {
 
@@ -8,5 +9,8 @@ public class GroceryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
