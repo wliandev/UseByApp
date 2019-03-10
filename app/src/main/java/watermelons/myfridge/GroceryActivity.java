a8e2ed7a10b9707f68299deede7e08b19a4154b7
lilliw2-admin <lilliw2-admin@ET201-14.inst.hssoe.uci.edu>
2019-03-09 20:50:30 -0800

    added date picker dialog

--- app/src/main/java/watermelons/myfridge/GroceryActivity.java
+++ app/src/main/java/watermelons/myfridge/GroceryActivity.java
@@ -1,19 +1,26 @@
 package watermelons.myfridge;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class GroceryActivity extends AppCompatActivity {
 
     private ArrayList<Food> groceryList;
     private ArrayAdapter<Food> groceryAdapter;
     private ListView groceryListView;
+    private DatePickerDialog datePickerDialog;
+    private Calendar calendar;
+    private Calendar expdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,35 @@ public class GroceryActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.grocery_toolbar);
         setSupportActionBar(myToolbar);
 
+    }
+
+    public Food searchItem(String string) {
+        for (Food f : groceryList){
+            if (f.getName().equals(string)){
+                return f;
+            }
+        }
+        return null;
+    }
+
+
+    public void itemClicked(View view){
+        CheckBox checkBox = (CheckBox)view;
+        if (checkBox.isChecked()){
+            final int month,day_of_month,year;
+            calendar = calendar.getInstance();
+            year = calendar.get(Calendar.YEAR);
+            month = calendar.get(Calendar.MONTH);
+            day_of_month = calendar.get(Calendar.DAY_OF_MONTH);
+            datePickerDialog = new DatePickerDialog(GroceryActivity.this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                            expdate.set(year,month,dayOfMonth);
+                        }
+                    }, year, month, day_of_month);
+            datePickerDialog.show();
+        }
     }
 
     public void toPantryActivity(View view){
